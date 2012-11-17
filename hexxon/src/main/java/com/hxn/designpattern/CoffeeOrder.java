ee736315de08bbdbd73385c95164dfe66b043fd2
Tomasz Banas <t.banas@gmail.com>
2012-11-17 19:49:12 +0100

    Added Design Pattern - Flyweight

---
+++ hexxon/src/main/java/com/hxn/designpattern/CoffeeOrder.java
@@ -0,0 +1,6 @@
+package com.hxn.designpattern;
+
+//Flyweight object interface
+public interface CoffeeOrder {
+public void serveCoffee(CoffeeOrderContext context);
+}
---
+++ hexxon/src/main/java/com/hxn/designpattern/CoffeeOrderContext.java
@@ -0,0 +1,13 @@
+package com.hxn.designpattern;
+
+public class CoffeeOrderContext {
+private int tableNumber;
+
+public CoffeeOrderContext(int tableNumber) {
+    this.tableNumber = tableNumber;
+}
+
+public int getTable() {
+    return this.tableNumber;
+}
+}
