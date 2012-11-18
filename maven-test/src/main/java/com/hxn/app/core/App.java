4b23272f9fa8419fae382852f31807ea537392ce
Tomasz Banas <t.banas@gmail.com>
2012-11-18 20:10:10 +0100

    reconfigure manifest configuration to using mainclass copy dependency jar to dependecy-jars folder

--- maven-test/src/main/java/com/hxn/app/core/App.java
+++ maven-test/src/main/java/com/hxn/app/core/App.java
@@ -1,13 +1,22 @@
 package com.hxn.app.core;
 
-/**
- * Hello world!
- *
- */
+import org.apache.log4j.Logger;
+
 public class App 
 {
+	private static final Logger  logger = Logger.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "maven-test:" );
+        System.out.println("Log4j ...");
+        for(int i=1 ; i<10; i++) {
+            System.out.println("Counter = " + i);
+              logger.debug("App message with DEBUG level");
+              logger.info("App message with INFO level ");
+              logger.warn("App message with WARN level ");
+              logger.error("App message with ERROR level ");
+              logger.fatal("App message with FATAL level ");
+        }
     }
 }
